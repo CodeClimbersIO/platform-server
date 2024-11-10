86b76be65baa0769a43c1e106c3c1e0746da217f
Paul Hovley <rphovley@gmail.com>
2024-11-10 13:39:58 -0700

    fix error report that gets sent to discord

--- src/routes/errorReportRoutes.ts
+++ src/routes/errorReportRoutes.ts
@@ -9,34 +9,48 @@ type ErrorReport = Record<string, unknown>
 const sendErrorToDiscord = async (error: ErrorReport) => {
   const webhookUrl = Deno.env.get('DISCORD_ERROR_WEBHOOK')
   assert(webhookUrl, 'DISCORD_ERROR_WEBHOOK is not set')
+  const email = error.userEmail || 'Unknown'
 
-  await fetch(webhookUrl, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      content: '**New Error Report**',
-      embeds: [
-        {
-          title: 'Error Details',
-          description: '```' + JSON.stringify(error, null, 2) + '```',
-          color: 15158332, // Red color
-        },
-      ],
-    }),
-  })
+  const errorDescription = {
+    error: error.error,
+    email,
+    errorStack: error.errorStack,
+  }
+  try {
+    await fetch(webhookUrl, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        content: '**New Error Report Sent By User: ' + email +
+          '**\nFull report: ',
+        embeds: [
+          {
+            title: 'Error Details',
+            description: '```' + JSON.stringify(errorDescription, null, 2) +
+              '```',
+            color: 15158332,
+          },
+        ],
+      }),
+    })
+  } catch (err) {
+    console.error('Error sending error to Discord:', err)
+  }
 }
 
 router.post('/discord', async (ctx: Context) => {
-  const { error } = ctx.request.body
-
+  const error = ctx.request.body
   if (!error) {
     ctx.response.status = 400
     ctx.response.body = {
       error: 'error is required',
     }
   }
-
   await sendErrorToDiscord(error)
+  ctx.response.status = 200
+  ctx.response.body = {
+    message: 'Error sent to Discord',
+  }
 })
 
-export const errorReportRouter = router
+export const errorReportRouter = router.prefix('/error-report')
